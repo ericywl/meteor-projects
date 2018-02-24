73688ec103ba848e7efcf59485b97ce85b0322f8
ericywl <midnight2903@gmail.com>
2018-02-25 00:00:56 +0800

    creating db

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,5 +1,37 @@
-import someDefault, {greetUser, name} from "../imports/utils";
+import React from "react";
+import ReactDOM from "react-dom";
+import {Meteor} from "meteor/meteor";
 
-console.log("Log from /client/main.js");
-console.log(greetUser() + " " + name);
-console.log(someDefault);
+const players = [{
+    _id: '1',
+    name: "Eric",
+    score: 10
+}, {
+    _id: '2',
+    name: "Ayy",
+    score: 1
+}, {
+    _id: '3',
+    name: "Andrew",
+    score: -14
+}];
+
+const renderPlayers = function (playersList) {
+    return playersList.map(function (player) {
+        let pointStr = (player.score == 1) ? "point" : "points";
+        return <p key={player._id}>{player.name} has {player.score} {pointStr}.</p>
+    });
+};
+
+Meteor.startup(function () {
+    let title = "Account Settings";
+    let name = "Eric";
+    let jsx = (
+        <div>
+            <h1>{title}</h1>
+            <p>Hello {name}!</p>
+            <p>Second paragraph here!</p>
+            {renderPlayers(players)}
+        </div>);
+    ReactDOM.render(jsx, document.getElementById("app"));
+});
--- score-keep/imports/math.js
+++
@@ -1,5 +0,0 @@
-console.log("Log from /imports/math.js");
-
-export let add = function (num1, num2) {
-    return num1 + num2;
-};
